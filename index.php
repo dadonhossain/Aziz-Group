1f3cb1f4294b2adad975fe0b9e366771faa9476b
dadonhossain <36604722+dadonhossain@users.noreply.github.com>
2021-05-28 21:56:11 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en-US">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width", initial-scale=1.0" />
+      <title>Aziz Group of Companies Ltd.</title>
+      <link rel="stylesheet" href="css/components.css">
+      <link rel="stylesheet" href="css/responsee.css">
+      <link rel="stylesheet" href="owl-carousel/owl.carousel.css">
+      <link rel="stylesheet" href="owl-carousel/owl.theme.css">
+      <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+      <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+      <script type="text/javascript" src="js/jquery-ui.min.js"></script>    
+      <script type="text/javascript" src="js/modernizr.js"></script>
+      <script type="text/javascript" src="js/responsee.js"></script>
+      <!--[if lt IE 9]>
+	      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+      <![endif]-->
+   </head>
+   <body class="size-960">
+      <!-- HEADER -->
+      <header>
+         <div class="line">
+            <div class="box">
+               <div class="s-6 l-2">
+                  <img src="img/1.png">
+               </div>
+			   <!-- <div class="s-0 1-2">  -->
+			      <!-- <img src="img/2.png">  -->
+			   <!-- </div>  -->
+            </div>
+         </div>
+         <!-- TOP NAV -->  
+         <div class="line">
+            <nav class="margin-bottom">
+               <p class="nav-text">Main menu</p>
+               <div class="top-nav s-12 l-10">
+                  <ul>
+                     <li><a>Home</a></li>
+                     <li>
+                        <a>Business</a>
+                        <ul>
+						   <li><A>R.H Corporation</A></li>
+						   <li><A>H.S Corporation</A></li>
+                           <li><A href = "http://182.163.102.230:8181/apex/f?p=107" target="_blank">SAS Enterprise</A></li>
+						   <li><A>Shahida Trading</A></li>
+						   <li><A>RJM Corporation</A></li>
+						   <li><A>Aziz Court</A></li>
+						   <li><A>AAA Corporation</A></li>
+                        </ul>
+                     </li>
+                     <li>
+                        <a>Company</a>
+                        <ul>
+                           <li><a>About</a></li>
+                           <li><a>Location</a></li>
+                        </ul>
+                     </li>
+                     <li><a>Contact</a></li>
+					 <li><a>Services</a>
+					 <ul>
+					 <li><A href = "http://182.163.102.230:8181/apex/f?p=100" target="_blank">Visit Report</A></li>
+					 <li><a href = "http://182.163.102.230:8181/apex/f?p=100" target="_blank">Leave Application</a></li>
+					 <li><a>Download Form</a>
+						 <ul>
+						     <li><a href="document/AG-OPHRM-Gen-08-1 __ Travel Request Form.pdf" download="AG-OPHRM-Gen-08-1 __ Travel Request Form.pdf">Travel Request Form</a></li>
+							 <li><a href="document/Hierarchy.pdf" download="Hierarchy.pdf">Hierarchy</a></li>
+							 <li><a href="document/AG-OPHRM-Gen-01 __ Requisition Form.pdf" download="AG-OPHRM-Gen-01 __ Requisition Form.pdf">Requisition Form</a></li>
+							 <li><a href="document/AG-OPHRM-Gen-02 __ Request for Corporate Mobile Number.pdf" download="AG-OPHRM-Gen-02 __ Request for Corporate Mobile Number.pdf">Request for Corporate Mobile Number</a></li>
+							 <li><a href="document/AG-OPHRM-Gen-03 __ OT FORM.pdf" download="AG-OPHRM-Gen-03 __ OT FORM.pdf">OT FORM</a></li>
+							 <li><a href="document/AG-OPHRM-Gen-04 __ OT Claim Form for Rest Day & Govt. Holidays.pdf" download="AG-OPHRM-Gen-04 __ OT Claim Form for Rest Day & Govt. Holidays.pdf">OT Claim Form for Rest Day & Govt. Holidays</a></li>
+							 <li><a href="document/AG-OPHRM-Gen-05 __ Employee Allowance Form (Marriage).pdf" download="AG-OPHRM-Gen-05 __ Employee Allowance Form (Marriage).pdf">Employee Allowance Form (Marriage)</a></li>
+							 <li><a href="document/AG-OPHRM-Gen-06 __ Salary Advance Form.pdf" download="AG-OPHRM-Gen-06 __ Salary Advance Form.pdf">Salary Advance From</a></li>
+							 <li><a href="document/AG-OPHRM-Gen-07 __ Delivery Chalan.pdf" download="AG-OPHRM-Gen-07 __ Delivery Chalan.pdf">Delivery Chalan</a></li>
+							 <li><a href="document/AG-OPHRM-IT-01 __  IT Trouble Report Request Form.pdf" download="AG-OPHRM-IT-01 __  IT Trouble Report Request Form.pdf">IT Trouble Report Request Form</a></li>							 
+						 </ul>
+					 </li>
+					 </ul>
+                  </ul>
+               </div>
+               <div class="top-large-social-block">
+                  <a href="http://facebook.com"><span class="social facebook"></span></a>
+               </div>
+            </nav>
+         </div>
+      </header>
+      <section>
+         <!-- CAROUSEL -->  
+         <div class="line">
+            <div id="owl-demo" class="owl-carousel owl-theme  margin-bottom">
+               <div class="item"><img src="img/big_1.jpg" alt=""></div>
+               <div class="item"><img src="img/big_2.jpg" alt=""></div>
+               <div class="item"><img src="img/big_3.jpg" alt=""></div>
+			   <div class="item"><img src="img/big_4.jpg" alt=""></div>
+			   <div class="item"><img src="img/big_5.jpg" alt=""></div>
+            </div>
+         </div>
+         <!-- HOME PAGE BLOCK -->      
+         <div class="line">
+            <div class="margin">
+               <div class="s-12 m-6 l-4 margin-bottom">
+                  <div class="box">
+                     <h2>About</h2>
+                     <p>Link is under constraction</p>
+                  </div>
+               </div>
+               <div class="s-12 m-6 l-4 margin-bottom">
+                  <div class="box">
+                     <h2>Company</h2>
+                     <p>Link is under constraction</p>
+                  </div>
+               </div>
+               <div class="s-12 m-12 l-4 margin-bottom">
+                  <div class="box">
+                     <h2>Services</h2>
+                     <p>Link is under constraction</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- ASIDE NAV AND CONTENT -->
+         <div class="line">
+            <div class="box margin-bottom">
+               <div class="margin">
+                  <!-- CONTENT -->
+                  <article class="s-12 m-7 l-8">
+                     <h1>Content</h1>
+                     <p>Aziz Group is a diversified chemical company with the motto of bringing innovation and state-of-the-art modern technology
+					 with safety concerns to constantly deliver chemical solutions. The Company delivers a broad range of products and service 
+					 ranging to customers in home and abroad, connecting chemistry and innovation with the principles of sustainability to help 
+					 provide everything from textile, garments, basic chemicals, pharmaceuticals to printing, chloro alchali, machineries and instruments.
+					 In 2008, Aziz Group had annual sales of USD 65 million and employed approximately 2000 people countrywide. 
+					 The Company has 2 manufacturing sites - one in the Comilla Export Processing Zone (CEPZ) and another in Sreepur, Gazipur, 
+					 one of the fastest growing manufacturing zone in the country. 
+                     </p>
+                  </article>
+                  <!-- ASIDE NAV -->
+                  <div class="s-12 m-5 l-4">
+                     <h3>Navigation</h3>
+                     <div class="aside-nav">
+                        <ul>
+                     <li><a>Home</a></li>
+                     <li>
+                        <a>Business</a>
+                        <ul>
+                           <li><A>R.H Corporation</A></li>
+						   <li><A>H.S Corporation</A></li>
+                           <li><A href = "http://182.163.102.230:8181/apex/f?p=107" target="_blank">SAS Enterprise</A></li>
+						   <li><A>Shahida Trading</A></li>
+						   <li><A>RJM Corporation</A></li>
+						   <li><A>Aziz Court</A></li>
+						   <li><A>AAA Corporation</A></li>                           
+                        </ul>
+                     </li>
+                     <li>
+                        <a>Company</a>
+                        <ul>
+                           <li><a>About</a></li>
+                           <li><a>Location</a></li>
+                        </ul>
+                     </li>
+                     <li><a>Contact</a></li>
+					 <li><a>Services</a>
+					 <ul>
+					 <li><A href = "http://182.163.102.230:8181/apex/f?p=100" target="_blank">Visit Report</A></li>
+					 <li><A href = "http://182.163.102.230:8181/apex/f?p=100" target="_blank">Leave Application</A></li>
+					 <li><a>Download Form</a>
+						 <ul>
+						     <li><a href="document/AG-OPHRM-Gen-08-1 __ Travel Request Form.pdf" download="AG-OPHRM-Gen-08-1 __ Travel Request Form.pdf">Travel Request Form</a></li>
+							 <li><a href="document/Hierarchy.pdf" download="Hierarchy.pdf">Hierarchy</a></li>
+							 <li><a href="document/AG-OPHRM-Gen-01 __ Requisition Form.pdf" download="AG-OPHRM-Gen-01 __ Requisition Form.pdf">Requisition Form</a></li>
+							 <li><a href="document/AG-OPHRM-Gen-02 __ Request for Corporate Mobile Number.pdf" download="AG-OPHRM-Gen-02 __ Request for Corporate Mobile Number.pdf">Request for Corporate Mobile Number</a></li>
+							 <li><a href="document/AG-OPHRM-Gen-03 __ OT FORM.pdf" download="AG-OPHRM-Gen-03 __ OT FORM.pdf">OT FORM</a></li>
+							 <li><a href="document/AG-OPHRM-Gen-04 __ OT Claim Form for Rest Day & Govt. Holidays.pdf" download="AG-OPHRM-Gen-04 __ OT Claim Form for Rest Day & Govt. Holidays.pdf">OT Claim Form for Rest Day & Govt. Holidays</a></li>
+							 <li><a href="document/AG-OPHRM-Gen-05 __ Employee Allowance Form (Marriage).pdf" download="AG-OPHRM-Gen-05 __ Employee Allowance Form (Marriage).pdf">Employee Allowance Form (Marriage)</a></li>
+							 <li><a href="document/AG-OPHRM-Gen-06 __ Salary Advance Form.pdf" download="AG-OPHRM-Gen-06 __ Salary Advance Form.pdf">Salary Advance From</a></li>
+							 <li><a href="document/AG-OPHRM-Gen-07 __ Delivery Chalan.pdf" download="AG-OPHRM-Gen-07 __ Delivery Chalan.pdf">Delivery Chalan</a></li>
+							 <li><a href="document/AG-OPHRM-IT-01 __  IT Trouble Report Request Form.pdf" download="AG-OPHRM-IT-01 __  IT Trouble Report Request Form.pdf">IT Trouble Report Request Form</a></li>
+						 </ul>
+					 </li>
+					 </ul>
+                  </ul>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- GALLERY CAROUSEL -->
+         <div class="line">
+            <h2>Gallery</h2>
+            <div id="owl-demo2" class="owl-carousel margin-bottom">
+               <div class="item"><img class="lazyOwl" data-src="img/small_1.jpg" alt=""></div>
+               <div class="item"><img class="lazyOwl" data-src="img/small_2.jpg" alt=""></div>
+               <div class="item"><img class="lazyOwl" data-src="img/small_3.jpg" alt=""></div>
+               <div class="item"><img class="lazyOwl" data-src="img/small_4.jpg" alt=""></div>
+               <div class="item"><img class="lazyOwl" data-src="img/small_5.jpg" alt=""></div>
+               <div class="item"><img class="lazyOwl" data-src="img/small_1.jpg" alt=""></div>
+               <div class="item"><img class="lazyOwl" data-src="img/small_2.jpg" alt=""></div>
+               <div class="item"><img class="lazyOwl" data-src="img/small_3.jpg" alt=""></div>
+               <div class="item"><img class="lazyOwl" data-src="img/small_4.jpg" alt=""></div>
+               <div class="item"><img class="lazyOwl" data-src="img/small_5.jpg" alt=""></div>
+               <div class="item"><img class="lazyOwl" data-src="img/small_1.jpg" alt=""></div>
+               <div class="item"><img class="lazyOwl" data-src="img/small_2.jpg" alt=""></div>
+               <div class="item"><img class="lazyOwl" data-src="img/small_3.jpg" alt=""></div>
+               <div class="item"><img class="lazyOwl" data-src="img/small_4.jpg" alt=""></div>
+               <div class="item"><img class="lazyOwl" data-src="img/small_5.jpg" alt=""></div>
+               <div class="item"><img class="lazyOwl" data-src="img/small_1.jpg" alt=""></div>
+            </div>
+         </div>
+      </section>
+      <!-- FOOTER -->
+      <footer class="line">
+         <div class="box">
+            <div class="s-12 l-6">
+               <p>Copyright 2015, Aziz Group of Companies Ltd.</p>
+            </div>
+            <div class="s-12 l-6">
+               <a class="right" href="http://www.data-innovator.blogspot.com" title="Data-innovator">Design and coding by Nasir Uddin</a>
+            </div>
+         </div>
+      </footer>
+      <script type="text/javascript" src="owl-carousel/owl.carousel.js"></script>  
+      <script type="text/javascript">
+         jQuery(document).ready(function($) {	  
+           $("#owl-demo").owlCarousel({		
+         	navigation : true,
+         	slideSpeed : 300,
+         	paginationSpeed : 400,
+         	autoPlay : true,
+         	singleItem:true
+           });
+           $("#owl-demo2").owlCarousel({
+         	items : 4,
+         	lazyLoad : true,
+         	autoPlay : true,
+         	navigation : true,
+         	pagination : false
+           });
+         });	 
+      </script>
+   </body>
+</html>
